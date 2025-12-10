2f40f7633a3f6f1ad17590dbc5f10118a5d40af7
Graeme Folk <graemefolk@gmail.com>
2025-12-09 21:32:44 -0700

    fix: update the skeleton cards to align with the stlying of existing team cards, respect theme

--- client/src/app/shared/skeleton-card/skeleton-card.component.ts
+++ client/src/app/shared/skeleton-card/skeleton-card.component.ts
@@ -1,9 +1,31 @@
 import { Component, input } from "@angular/core";
+import { MatIconButton } from "@angular/material/button";
+import {
+  MatCard,
+  MatCardActions,
+  MatCardContent,
+  MatCardHeader,
+  MatCardSubtitle,
+  MatCardTitle,
+} from "@angular/material/card";
+import { MatDivider } from "@angular/material/divider";
+import { MatIcon } from "@angular/material/icon";
 
 @Component({
   selector: "app-skeleton-card",
   templateUrl: "./skeleton-card.component.html",
   styleUrls: ["./skeleton-card.component.scss"],
+  imports: [
+    MatCard,
+    MatCardHeader,
+    MatCardTitle,
+    MatCardSubtitle,
+    MatCardContent,
+    MatCardActions,
+    MatDivider,
+    MatIconButton,
+    MatIcon,
+  ],
 })
 export class SkeletonCardComponent {
   readonly showTransactions = input(false);
